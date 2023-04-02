214a3af1daa2a120d28d7b2fe70fac47f982ad5e
alina-marhaniia <115729722+alina-marhaniia@users.noreply.github.com>
2023-04-03 00:58:07 +0300

    added lodash throttle

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,11 +1,12 @@
 import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
 const iframe = document.querySelector('iframe');
-const player = new Player(iframe);
+const player = new Vimeo.Player(iframe);
 
-player.on('timeupdate', function(data) {
+player.on('timeupdate', throttle(function(data) {
   localStorage.setItem('videoplayer-current-time', data.seconds);
-});
+}, 1000));
 
 const currentTime = localStorage.getItem('videoplayer-current-time');
 if (currentTime) {
