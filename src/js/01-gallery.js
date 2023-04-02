2fbae183ce1fa04488aa604caac56a90bd1648c7
alina-marhaniia <115729722+alina-marhaniia@users.noreply.github.com>
2023-04-03 00:16:59 +0300

    task 1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,4 +2,35 @@
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
+import 'simplelightbox/dist/simple-lightbox.min.css';
+import SimpleLightbox from 'simplelightbox';
+
+const galleryEl = document.querySelector('.gallery');
+
+const galleryMarkup = galleryItems
+  .map(({ preview, original, description }) => {
+    return `
+       <li class="gallery__item">
+       <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      alt="${description}"
+    />
+  </a>
+</li>
+      `;
+  })
+  .join('');
+
+galleryEl.insertAdjacentHTML('beforeend', galleryMarkup);
+
+const gallery = new SimpleLightbox('.gallery a', {
+    captions: true,
+    captionDelay: 250
+});
+
+
+
+
 console.log(galleryItems);
