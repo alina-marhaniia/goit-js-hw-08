6f0b0a9e7d75c2e274bd95747a8009dbec0e2eb1
alina-marhaniia <115729722+alina-marhaniia@users.noreply.github.com>
2023-04-03 00:43:28 +0300

    task 2 done

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,13 @@
+import Player from '@vimeo/player';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+player.on('timeupdate', function(data) {
+  localStorage.setItem('videoplayer-current-time', data.seconds);
+});
+
+const currentTime = localStorage.getItem('videoplayer-current-time');
+if (currentTime) {
+  player.setCurrentTime(currentTime);
+}
