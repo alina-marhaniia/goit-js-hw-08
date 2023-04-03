bfb1291b48157608e254bce5c4308a7ccfed78e6
alina-marhaniia <115729722+alina-marhaniia@users.noreply.github.com>
2023-04-03 17:16:15 +0300

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -2,7 +2,7 @@ import Player from '@vimeo/player';
 import throttle from 'lodash.throttle';
 
 const iframe = document.querySelector('iframe');
-const player = new Vimeo.Player(iframe);
+const player = new Player(iframe);
 
 player.on('timeupdate', throttle(function(data) {
   localStorage.setItem('videoplayer-current-time', data.seconds);
